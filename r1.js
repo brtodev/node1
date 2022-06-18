67779a527cbf447b25c7ca5d8543df5a3931f152
brtodev <103844644+brtodev@users.noreply.github.com>
2022-06-18 19:30:16 +0300

    Update and rename README.md to r1.js

---
+++ r1.js
@@ -0,0 +1,11 @@
+const pi = 3.14;
+
+const yariCap = process.argv.slice(2);
+
+function daireHesap(yariCap){
+  const alan = pi * (yariCap * yariCap);
+  console.log(`Alan: ${alan}`)
+
+}
+
+daireHesap(Yaricap);
